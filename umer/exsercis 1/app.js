7900e7fe870f1c99ee65204175331abd1ee8ca7d
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-17 11:38:44 +0500

    exsercise 1

---
+++ umer/exsercis 1/app.js
@@ -0,0 +1,18 @@
+// 1.	Write a script to greet your website visitor using JS alert box.
+alert("welcom")
+// 2.	Write a script to display following message on your web page:
+alert("Error ! pleas enter a valid password")
+// 3.	Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to js land... \n  Happy coding!")
+// 4.	Write a script to display following messages in sequence:
+alert("Welcome to js land") /* sir agla part nhi arha */
+// 5.	Generate the following message through browser’s developer console:
+alert("Hello... I can run js through my web browser's console"); /* & show in html */
+// 6.	Make use of alerts in your new/existing HTML & CSS project.
+// i use already alert in my html & css project
+// 7.	Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a.	Head
+// b.	Body (before your page’s HTML)
+// c.	Body (inside your page’s HTML)
+// d.	Body (after your page’s HTML)
+  /* answer.7 show in html */
