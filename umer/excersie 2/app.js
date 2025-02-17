036be6634b772e8f8972f2e8f1d09823c93d2d99
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-17 13:01:52 +0500

    excersie 2

---
+++ umer/excersie 2/app.js
@@ -0,0 +1,22 @@
+// 1. Declare a variable called username
+let username;
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+let myname = ("UMER FAROOQ");
+// 3. Write a script to 
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box 
+let message = ("Hello World");
+alert(message);
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. Example:
+let studentname = ("UMER FAROOQ");
+let studentage = ("18 years old");
+let studentinsitude = ("SMIT")
+let studentcourse = ("web development")
+alert(studentname)
+alert(studentage)
+alert(studentinsitude)
+alert(studentcourse)
+// 5. Write a script to display the following alert using one JS variable:
+let food = ("PIZZA \n PIZZ \n PIZ \n PI \n p");
+alert(food);
