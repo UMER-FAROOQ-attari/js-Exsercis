b333e757664c806746217a58219b67b38d4d6450
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-18 08:20:58 +0500

    Add files via upload

---
+++ umer/exsersie 3/app.js
@@ -0,0 +1,19 @@
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+let my_age = 18;
+alert("I am "+ my_age+ " Years old");
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+let visitCount = 21;
+alert("You have visited this site "+ visitCount+" times");
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+let birthYear = 2006;
+document.write("My birth Year is " +birthYear+ " <br> ");
+document.write("Data  type of my declared variable is " + typeof(birthYear));
+// 4. A visitor visits an online clothing storewww.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name 
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+let visitor_name = ("ABUBAKER ATTARI");
+let Product_title = (" books");
+let Quantity = 10;
+let store = ("UM store");
+document.write( "<br>" + visitor_name + " ordered " + Quantity + Product_title +" on " + store);
