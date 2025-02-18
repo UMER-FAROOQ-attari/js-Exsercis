86a8b7027a8d5e19091720be8e2e458e5a9a6ee9
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-18 15:09:50 +0500

    Add files via upload

---
+++ umer/exercise 5-7/app.js
@@ -0,0 +1,14 @@
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+let age_1 = 13;
+let age_2 = 9;
+let Sum_age = age_1 + age_2;
+alert("The Sum of " + age_1 + " and " + age_2 + " is " + Sum_age);
+// 2. Repeat task1 for subtraction, multiplication, division & modulus. E.g
+let subtraction_age = age_1 - age_2;
+alert("The result of subtraction " + age_1 +" from " + age_2 + " is " + subtraction_age);
+let multiplication_age = age_1 * age_2 ;
+alert("The result of multiplying "+age_1+" by " +age_2 + " is "+multiplication_age);
+let division_age = age_1 / age_2;
+alert("The result if dividing " + age_1 + " by " + age_2 + " is approximately " + division_age);
+let modulus_age = age_1 % age_2;
+alert("The remainder when " + age_1 + " is divided by " + age_2 + " is "+ modulus_age)
