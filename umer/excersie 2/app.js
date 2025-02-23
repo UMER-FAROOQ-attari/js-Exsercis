0184ec865105e90a7879fb1127aab1c4cc967e66
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-23 23:12:37 +0500

    Update app.js

--- umer/excersie 2/app.js
+++ umer/excersie 2/app.js
@@ -1,9 +1,9 @@
-// 1. Declare a variable called username
+// 1. Declare a variable called username.
 let username;
 // 2. Declare a variable called myName & assign to it a string that represents your Full Name.
 let myname = ("UMER FAROOQ");
-// 3. Write a script to 
-// a. declare a JS variable, titled message
+// 3. Write a script to .
+// a. declare a JS variable, titled message.
 // b. assign “Hello World” to the variable message
 // c. display the message in alert box 
 let message = ("Hello World");
