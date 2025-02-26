2100710582cf2bc312d2d8f0757bed1f89889a71
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-02-26 09:48:21 +0500

    Add files via upload

--- umer/exsercise 14-17/app.js
+++ umer/exsercise 14-17/app.js
@@ -53,33 +53,117 @@ document.write("<br>"+"First index of the array : "+ car.indexOf("Audi"))
 document.write("<br>"+ "Car at fist index of the array : "+ car[0] );
 document.write("<br>"+ "Last index of the arry ;" + car[car.length -1]);
 document.write("<br>"+ "Car at last index of the array : " + (car.length -1))
-
-// 1. Create an Object:
-// Create an object called car with properties: make, model, year, and color. Assign appropriate values to each property.
-
-// 2. Accessing Object Properties:
-// Given the following object:
-
-// const user = {
-//     name: "John",
-//     age: 30,
-//     email: "john.doe@example.com"
-// };
-// Write code to print out the user's name and email to the console.
-
-// 3. Update Object Property:
-// Using the object car you created earlier, update the color property to red and log the updated object to the console.
-
-// 4. Adding New Properties to an Object:
-// Add a new property price to the car object, and assign it a value of 20000. Print the updated object.
-
-// 5. 
-// Create an object called person with properties firstName and lastName. print first name + last name.
-// 7. Selecting Multiple Elements:
-// Write a JavaScript function that selects all the paragraphs (<p>) on a webpage and change their text to something else.
-
-// 8. Looping Through NodeList:
-// Using document.querySelectorAll(), select all the list items (<li>) with the class menu-item and log each one to the console.
-
-// 9. Manipulating Text Content:
-// Select all the <h2> tags on the page and change their text content to "Updated Title".
+// 11. Write a program to store 3 student names in an array. Take
+// another array to store score of these three students. Assume
+// that total marks are 500 for each student, display the scores &
+// percentages of students like:
+let students = [ "Michael" , "John" , "Tony"];
+let numbers = [320,230,480 ];
+let total_number = 500;
+document.write( "<br>"+ " Score of " + students[0] + " is " + numbers[0] + ". percentages : " +  numbers[0] / total_number  * 100 +"%" );
+document.write( "<br>"+ " Score of " + students[1] + " is " + numbers[1] + ". percentages : " +  numbers[1] / total_number  * 100 +"%" );
+document.write( "<br>"+ " Score of " + students[2] + " is " + numbers[2] + ". percentages : " +  numbers[2] / total_number  * 100 +"%" );
+// 12. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end &
+// add that color to the end of the array. Display the updated
+// array in your browser.
+// c. Add two more color to the beginning of the array. Display
+// the updated array in your browser.
+// ARRAYS |	JAVASCRIPT
+// Page	4 of	8
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color &
+// color name. Then add the color to desired position/index.
+// . Display the updated array in your browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+let colors = [ "red" , "green", "yellow" , "pink"];
+document.write("</br>"+ colors);
+colors.unshift("orange")
+document.write("</br>"+colors);
+colors.push("golden")
+document.write("</br>"+colors);
+colors.unshift("zink", "black");
+document.write("</br>"+colors);
+colors.shift()
+document.write("</br>"+colors);
+colors.pop()
+document.write("</br>"+colors);
+colors.splice(3,0,"white")
+document.write("</br>"+colors);
+colors.splice(5,2)
+document.write("</br>"+colors);
+// 13. Write a program to store student scores in an array & sort
+// the array in ascending order using Array’s sort method. 
+let scores = [320, 230 ,480,120];
+document.write("</br>"+"Scores of Students : " + scores);
+scores.sort((a, b) => a - b);
+document.write("</br>"+"assaending Scores of Students : " + scores);
+// 15. Write a program to initialize an array with city names. Copy
+// 3 array elements from cities array to selectedCities array. 
+let cities = [ "karachi" , "lahore" , "toba tek sing", "Islamabad", "Quetta", "Peshawar"] 
+document.write("</br>"+cities);
+let cities_3 = cities.slice(2,5)
+document.write("</br>"+cities_3);
+// 16. Write a program to create a single string from the below
+// mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+var arr = ["This ", " is ", " my ", " cat"];
+let arrs= arr.join("")
+document.write("</br>"+arrs);
+// 17. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they were
+// stored. (FIFO-First In First Out)
+let outs = []
+outs.push("keyborad")
+outs.push("Mouse")
+outs.push("Printer")
+outs.push("Moniter");
+document.write("</br>"+ outs)
+outs.shift()
+document.write("</br>"+ outs)
+outs.shift()
+document.write("</br>"+ outs)
+outs.shift()
+document.write("</br>"+ outs)
+outs.shift()
+document.write("</br>"+ outs)
+// 18. Create a new array. Store values one by one in such a way
+// that you can access the values in reverse order. (Last In-First
+// Out)
+outs.push("keyborad")
+outs.push("Mouse")
+outs.push("Printer")
+outs.push("Moniter");
+document.write("</br>"+ outs)
+outs.pop()
+document.write("</br>"+ outs)
+outs.pop()
+document.write("</br>"+ outs)
+outs.pop()
+document.write("</br>"+ outs)
+outs.pop()
+document.write("</br>"+ outs)
+outs.pop()
+// 21. Declare and initialize a multidimensional array representing
+// the following matrix:
+let multidimensional = [
+    [0,1,2,3],
+    [1,0,1,2],
+    [2,1,0,1]
+]
+document.write("</br>"+ multidimensional)
+let m_join = multidimensional.join()
+document.write("</br>"+m_join)
+document.write("")
