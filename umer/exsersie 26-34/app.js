e80ad5ea6d08a45e9474a884c3850ee276cbafbc
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-05-14 00:52:14 +0500

    complete ful

--- umer/exsersie 26-34/app.js
+++ umer/exsersie 26-34/app.js
@@ -54,7 +54,17 @@ if (htnmb === 2 ){
 // 100 in your browser.
 let numb100 = (Math.random() * 99) + 1;
 numb100 = Math.round(numb100);
-document.write(`<br> random number between 1 to 100 is : ${numb100}` ) 
+document.write(`<br> random number between 1 to 100 is : ${numb100}` )
+//     9. Write a program that asks the user about his weight. Parse the
+// user input and display his weight in your browser. Possible user
+// inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+    let kg = prompt(`pleas enter your weight`);
+    kg = parseFloat(kg);
+    alert(kg)
 // 10. Write a program that stores a random secret number from 1
 // to 10 in a variable. Ask the user to input a number between 1
 // and 10. If the user input equals the secret number, congratulate
@@ -121,3 +131,64 @@ else if ( am_pm >= 12){
 }
 // 18. Write a program that creates a Date object for the last day of the
 // last month of 2020 and assigns it to variable named laterDate
+let s_date = new Date("July 04, 2025");
+alert(s_date);
+// 19. Create a date object of the starting date of this Ramadan and alert
+// the number of days past since 1st Ramadan?
+// Note: 1st Ramadan was on June 18, 2015
+let ramd_day = new Date("June 18, 2015");
+ramd_day = ramd_day.getTime();
+let right_now = new Date();
+right_now = right_now.getTime();
+let rem_day = right_now -ramd_day ;
+rem_day = rem_day/1000/60/60/24;
+rem_day = Math.round(rem_day)
+alert(`${rem_day} days have passed since 1st Ramdan , 2015`);
+// 20. Write a program that displays in your browser the seconds that
+// elapsed between the reference date and the beginning of 2015.
+let today_time = new Date();
+let baqi_time = new Date("January 01, 2025");
+baqi_time = baqi_time.getTime();
+let tod_sec = today_time.getTime();
+let baq_time = tod_sec - baqi_time;
+console.log(`On reference date ${today_time}, <br> ${baq_time} seconds had passed sice beginning 0f 2025 `)
+// 21. Create a Date object for the current date and time. Extract the
+// hours, reset the date object an hour ahead and finally display the
+// date object in your browser.
+let abi_time = new Date();
+let ikganta = new Date(abi_time)
+ikganta.setHours(ikganta.getHours() - 1 )
+console.log(`Current date : ${abi_time} <br> 1 hours ago, its was ${ikganta}`);
+// 22. Write a program that creates a date object and show the date
+// in an alert box that is reset to 100 years back?
+let abi = new Date();
+let soo = new Date(abi)
+soo.setFullYear(soo.getFullYear ()- 100);
+alert(soo)
+// 23. Write a program to ask the user about his age. Calculate and
+// show his birth year in your browser.
+let age = +prompt(` write  your age `);
+let nedate = new Date();
+nedate = nedate.getFullYear();
+let answer = nedate - age;
+alert(answer)
+// 24. Write a program to generate your K-Electric bill in your
+// browser. All the amounts should be rounded off to 2 decimal
+// places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+let cus_name = prompt(`pleas enter your nme`)
+let mont = new Date();
+mont = mont.getMonth();
+let ary = ["jan", "feb", "march","april","may","june"]
+let nmb_unit = +prompt(`pleas enter number of units`);
+let char_per_unit = +prompt(`pleas enter charges per unit`);
+let nap = nmb_unit * char_per_unit ;
+let lps = 500;
+let gap = nap + lps;
+document.write(`custmer name : ${cus_name} <br> Month : ${ary[mont]} <br> Number of Unit : ${nmb_unit}<br> charges per Unit ${char_per_unit} <br>net amount payable : ${nap}<br> late payment surechge : ${lps} <br> Gross Amount payable : ${gap}`)
