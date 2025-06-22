f0d44c171006c2f9850e0816575d8561c1aca5fb
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-06-22 16:33:29 +0500

    next Question complete

--- umer/exsersie 38-42/java.js
+++ umer/exsersie 38-42/java.js
@@ -46,3 +46,61 @@ let b = +prompt("Enter side b:");
 let c = +prompt("Enter side c:");
 let areaResult = calculateArea(a, b, c);
 alert("Area of triangle is: " + areaResult.toFixed(2));
+// 4. Write a function that receives marks received by a student in 3
+// subjects and returns the average and percentage of these
+// marks. there should be 3 functions one is the mainFunction
+// and other are for average and percentage. Call those functions
+// from mainFunction and display result in mainFunction
+let english = +prompt("Pleas enter your english number . . .");
+let math = +prompt("Pleas enter your math number . . .");
+let fiqh = +prompt("Pleas enter your fiqh number . . .");
+function main(){
+    let total = english + math + fiqh;
+    
+    
+
+    function average( a ,b , c){
+    let average = (a+ b+c)/3;
+    return(average)  
+    }
+    function percentage(nume){
+   return  ( nume / 300) * 100
+    }
+    document.write(`<br><br>Your english Number is :${english}`)
+    document.write(`<br>Your math Number is :${math}`)
+    document.write(`<br>Your fiqh Number is :${fiqh}`)
+    document.write(`<br><br>Your Numbers averge is :${average(english, math , fiqh).toFixed(2)}`)
+
+    document.write(`<br>Your  persentage is :${percentage(total).toFixed(2)}`)
+  
+    }
+
+
+main()
+// 5. You have learned the function indexOf. Code your own custom
+// function that will perform the same functionality. You can code
+// for single character as of now.
+let astring = prompt("Enter a string (e.g. banana):").toLowerCase();
+let wordsearch = prompt("Enter a letter to find index:").toLowerCase();
+
+function indexnumber(word) {
+  let indexnumb = -1;   
+  let arrystring = astring.split("");
+  let i = 0;
+
+  while(i < arrystring.length){
+    switch(arrystring[i]){
+      case word:
+        indexnumb = i;
+        return indexnumb;  // Match milte hi return
+    }
+    i++;
+  }
+
+  return indexnumb; // agar match na mile to -1
+}
+
+let anse = indexnumber(wordsearch);
+document.write(`Index of '${wordsearch}' is: ${anse}`);
+// 6. Write a function to delete all vowels from a sentence. Assume
+// that the sentence is not more than 25 characters long.
